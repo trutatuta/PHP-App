8b7e76952c41d26877c8105cc46d11a5de8043d5
Bohdan <ggmmaaiill.bg@gmail.com>
2021-03-11 20:13:30 +0100

    Session test

---
+++ from.php
@@ -0,0 +1,9 @@
+<?php
+
+session_start();
+
+$_SESSION["username"] = "lildan";
+
+?>
+
+<a href="index.php">First</a>
--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php ?>
+<?php
+
+session_start();
+
+if(!isset($_SESSION["username"])){
+    echo ("You must log in first");
+    ?>
+    <a href="from.php">Second</a>
+    <?php
+}else{
+    header("Location: home.php");
+    exit();
+}
+
+?>
