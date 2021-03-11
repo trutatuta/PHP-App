7e0153fcf30b2094635728e5aa21e102f2cadfdc
Bohdan <ggmmaaiill.bg@gmail.com>
2021-03-11 19:39:48 +0100

    DataBase class

---
+++ classes/DataBase.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace classes\DB;
+
+use mysqli;
+
+class DataBase{
+
+    private $mysqli;
+
+    function __construct($host, $username, $password, $db)
+    {
+        $this->mysqli = new mysqli($host, $username, $password, $db);
+
+        if($this->mysqli->connect_errno){
+            printf("ERROR %s\n", $this->mysqli->connect_error);
+            exit();
+        }
+    }
+
+    function __destruct()
+    {
+        $this->mysqli->close();
+    }
+
+}
