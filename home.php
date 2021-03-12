54b8da735b6e4c5f6b389b53c9c501f03c1544af
Bohdan <ggmmaaiill.bg@gmail.com>
2021-03-12 17:24:25 +0100

    Log Out

--- home.php
+++ home.php
@@ -3,5 +3,19 @@
 session_start();
 
 echo $_SESSION["username"];
+if(isset($_POST["logout"])){
+    session_unset();
+    session_destroy();
+    header("Location: index.php");
+    exit();
+}
+include("header.php");
+?>
 
+<form action="home.php" method="post">
+    <input type="submit" id="logout" name="logout" value="Log Out">
+</form>
+
+<?php
+include("footer.php");
 ?>
